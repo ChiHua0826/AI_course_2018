02814030f55f2eecb35ac421058d6aa49d55f881
Chi-Hua Chen <chihua0826@gmail.com>
2018-04-07 22:48:55 +0800

    Create linear regression (電信股價漲跌).r

---
+++ Regression/linear regression (電信股價漲跌).r
@@ -0,0 +1,28 @@
+#安裝neuralnet函式庫
+install.packages('neuralnet')
+#引用neuralnet函式庫
+library(neuralnet)
+
+#讀取訓練資料
+training_data <- read.csv(file.choose(), header = TRUE)
+
+name <- names(training_data)
+formula <- as.formula(paste("Class ~", 
+           paste(name[!name %in% "Class"], collapse = " + ")))
+
+#設定亂數種子
+set.seed(1)
+#訓練線性迴歸
+model <- neuralnet(formula, training_data, 
+                   hidden=c(0), linear.output = TRUE)
+#繪製線性迴歸
+plot(model)
+
+#讀取測試資料
+testing_data <- read.csv(file.choose(), header = TRUE)
+testing_data_input <- subset(testing_data, select = -Class)
+
+#將測試資料代入模型進行預測,並取得預測結果
+pred <- compute(model, testing_data_input)
+#運用表格呈現和比對預測結果
+table(round(pred$net.result), testing_data$Class)
